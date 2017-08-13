93d4ba005e740194b3c487462cb71384202f43e7
Hemanath-97 <30979359+Hemanath-97@users.noreply.github.com>
2017-08-13 23:25:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,46 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one' : { 
+        title : 'Article One | Ezhilavan',
+        content : `<p> This is my First Article`
+    },
+    'article-two' : {
+        title : 'Article two | Ezhilavan',
+        content : `<p> This is my Second Article`
+    },
+    'article-three' : {
+        title : 'Article One | Ezhilavan',
+        content : `<p> This is my First Article`
+    }
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var content = data.content;
+    
+    var template = `<html>
+    <head>
+     <title>
+      ${title}
+     </title>
+     <meta name="viewport" content="width=device-width, initial-scale=1" />
+     <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+     <div class="container">
+      <div>
+       <p> ${content}
+      </div> 
+     </div>
+    </body>
+ </html>`;
+ 
+ return template;
+      
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -13,8 +53,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-one',function(req,res){
-    res.send("Is here");
+app.get('/:articleName',function(req,res){
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/madi.png', function (req, res) {
